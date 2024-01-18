2eacd0885677429b0d93136f23416ef511523613
Felice <fishfillet2fab4u@gmail.com>
2024-01-18 19:29:49 +0000

    new functions added

--- src/index.js
+++ src/index.js
@@ -34,13 +34,55 @@ function draw() {
   });
 }
 
+function updateScore() {
+  score += barSize;
+  scoreCounter.innerText = score.toString().padStart(5, 0);
+}
+
+function onStack() {
+  currentRowIndex--;
+  // currentRowIndex = currentRowIndex - 1
+  barDirection = "right";
+
+  for (let i = 0; i < barSize; i++) {
+    gridMatrix[currentRowinex][i] = 1;
+  }
+}
+
+function mvoeRight(currentRow) {
+  currentRow.pop();
+  currentRow.unshift(0);
+}
+
+function moveLeft(currentRow) {
+  currentRow.shift();
+  currentRow.push(0);
+}
+
 function moveBar() {
   const currentRow = gridMatrix[currentRowIndex];
 
   if (barDirection === "right") {
-    currentRow.pop();
+    moveRight(currentRow);
+    const lastElement = crrentRow[currentRow.length - 1];
+    if (lastElement === 1) {
+      barDirection = "left";
+    }
+  } else if (barDirection === "left") {
+    moveLeft(currentRow);
+
+    const firstElement = currentRow[0];
+
+    if (firstElement === 1) {
+      barDirection = "right";
+    }
   }
 }
+function main() {
+  draw();
+  moveBar();
+}
 
-draw();
-moveBar();
+stackBtn.addEventListener("click", onStack);
+
+const gameInterval = setInterval(main, 600);
